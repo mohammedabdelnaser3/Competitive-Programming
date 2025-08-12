cc5b288b86789db3f38228ef7544cbabc031c0f7
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:22:39 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/SPOJ/ELIS/51678464_AC_60ms_5325kB.cpp
@@ -0,0 +1,104 @@
+/*
+*  Author:
+*
+* ╭━╮╭━╮╱╱╭━━━╮╭━━╮╱╭━━━╮╭━━━╮╭╮╱╱╱╭━╮╱╭╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮
+* ┃┃╰╯┃┃╱╱┃╭━╮┃┃╭╮┃╱╰╮╭╮┃┃╭━━╯┃┃╱╱╱┃┃╰╮┃┃┃╭━╮┃┃╭━╮┃┃╭━╮┃┃╭━━╯┃╭━╮┃
+* ┃╭╮╭╮┃╱╱┃┃╱┃┃┃╰╯╰╮╱┃┃┃┃┃╰━━╮┃┃╱╱╱┃╭╮╰╯┃┃┃╱┃┃┃╰━━╮┃╰━━╮┃╰━━╮┃╰━╯┃
+* ┃┃┃┃┃┃╱╱┃╰━╯┃┃╭━╮┃╱┃┃┃┃┃╭━━╯┃┃╱╭╮┃┃╰╮┃┃┃╰━╯┃╰━━╮┃╰━━╮┃┃╭━━╯┃╭╮╭╯
+* ┃┃┃┃┃┃╭╮┃╭━╮┃┃╰━╯┃╭╯╰╯┃┃╰━━╮┃╰━╯┃┃┃╱┃┃┃┃╭━╮┃┃╰━╯┃┃╰━╯┃┃╰━━╮┃┃┃╰╮
+* ╰╯╰╯╰╯╰╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰━━━╯╰╯╱╰━╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰╯╰━╯
+*
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+/*
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+*/
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg(Head H, Tail... T) { cerr << ' ' << H; dbg(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dx4[] = {-1, 0, 0, 1};
+int dy4[] = {0, -1, 1, 0};
+#define _ 0
+#define endl '\n'
+#define ll long long
+#define pii pair<int,int>
+#define ull unsigned long long
+#define all(X) (X).begin(),(X).end()
+#define allr(X) (X).rbegin(),(X).rend()
+#define Take(X) for (auto &it:(X)) cin >> it;
+#define Out(X) for (auto &it:(X))cout << it << " ";
+#define TAKE(X, S) for (auto &it:(X)) cin >> it, (S) += it;
+#define Fast_IO ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
+
+const int mod = 1e9 + 7;
+const double pi = acos(-1);
+const ll oo = LLONG_MAX;
+const int OO = 0x7f7f7f7f;
+/*___________________________________________________________________________*/
+#define int long long
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int,int>> vpi;
+#define ld long double
+const int N = 100;
+
+
+void blow_it() {
+    int n;
+    cin >> n;
+    vi v(n);
+    for(int &i: v) cin >> i;
+    int dp[25] = {};
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < v[i]; ++j){
+            dp[v[i]] = max(dp[v[i]], dp[j] + 1);
+        }
+    }
+
+
+    cout << *max_element(dp, dp + 24) << '\n';
+}
+
+void preprocess() {
+
+}
+
+int32_t main() {
+    Fast_IO;
+    freopen("erorr.txt", "w", stderr);
+//    freopen("blist.in", "r", stdin);b
+//    freopen("blist.out", "w", stdout);
+//    cout << fixed << setprecision(9);
+    int t = 1, cases = 1;
+    // cin >> t;
+    //std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+   preprocess();
+    while (t--) {
+        //cout << "Case #" << cases++ << ':' << endl;
+        blow_it();
+    }
+    //std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    //cerr << "Time : " << (end - start).count() / (1000000.0l) << " ms \n";
+    return (0 ^ _ ^ 0);
+}
