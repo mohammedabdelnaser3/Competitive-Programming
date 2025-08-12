cd0db494b0e668e4915eca7662b33d6eeb3332bd
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:26:21 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/CodeForces/489C/35372997_AC_15ms_8kB.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ()ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0) 
+
+int main(){
+int m, s, t, i;
+cin >> m >> s;
+string a, b;
+if (s == 0) { 
+cout << (m == 1 ? "0 0" : "-1 -1") << endl; return 0; 
+}
+for (i = 0; i < m; i++) {
+t = min(s, 9); 
+b += t + '0'; 
+s -= t;
+} 
+if (s > 0) { 
+cout << "-1 -1" << endl; 
+return 0; 
+} 
+for (i = m - 1; i >= 0; i--) 
+a += b[i];
+for (i = 0; a[i] == '0'; i++);
+a[i]--, a[0]++; 
+cout << a << " " << b << endl;
+return 0;
+}
