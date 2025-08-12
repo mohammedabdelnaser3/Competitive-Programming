aac85de92f9c27ca1309edaa22a272e4c16553d1
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:21:54 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/SPOJ/AGGRCOW/35396299_AC_90ms_5427kB.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fast() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+/*int digit(ll x){
+    ll ans=0;
+    while (x>0){
+        x/=10;
+        ans++;
+    }
+    return ans;
+}*/
+/*
+int main(){
+    fast();
+    ull a,b,x,lo=0,hi,mid;
+    cin>>a>>b>>x;
+    ull ans=0;
+        hi=x;
+        mid=lo+(hi-lo)/2;
+    while (lo<hi) {
+        if (ans>1000000000){
+            ans=1000000000;
+            break;
+        }
+        if ((a * mid) + (b * digit(mid)) < x) {
+            ans = mid;
+            lo = mid;
+            mid = lo + (hi - lo) / 2;
+        } else if ((a * mid) + (b * digit(mid)) > x) {
+            hi = mid - 1;
+            mid = lo + (hi - lo) / 2;
+        } else if ((a * mid) + (b * digit(mid)) == x) {
+            cout << mid;
+            return 0;
+        }
+        if( lo==mid || hi == mid)
+            break;
+    }
+    cout<<ans;
+    return 0;
+}*/
+bool check(int mid,int pos[],int n,int cows){
+    int ans=1;
+    int start=0;
+    for (int i = 1; i < n; ++i) {
+        if((pos[i]-pos[start])>=mid){
+            ans++;
+            start=i;
+            if (ans>=cows)
+                return true;
+        }
+    }
+        return false;
+}
+void bs(int n,int pos[],int cows){
+    int lo=1,hi=pos[n-1]-pos[0],ans;
+    while (lo<=hi){
+        int mid=(hi+lo)/2;
+        if (check(mid,pos,n,cows)){
+            ans=mid;
+            lo=mid+1;
+        }else {
+            hi=mid-1;
+        }
+    }
+    cout<<ans<<"\n";
+    return;
+}
+int main(){
+    int t;
+    cin>>t;
+    while (t--) {
+        int cows,n;
+        cin>>n>>cows;
+        int pos[n];
+        for (int i = 0; i < n; ++i)
+            cin>>pos[i];
+        sort(pos,pos+n);
+        bs(n,pos,cows);
+    }
+    return 0;
+}
