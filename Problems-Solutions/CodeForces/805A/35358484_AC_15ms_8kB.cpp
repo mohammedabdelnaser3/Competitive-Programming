4b4a791c47b58b345c8e375ef692e2f3c13a4d28
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:26:15 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/CodeForces/805A/35358484_AC_15ms_8kB.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fast() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main(){
+    int r,l;
+    cin>>r>>l;
+    if (r==l)
+        cout<<r;
+    else
+        cout<<2;
+    return 0;
+}
