13d0b92bf84349814a11c65a61d1aae0e33655c6
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:25:08 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/Gym/310855O/35845056_AC_15ms_8kB.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+#define f(x,n) for(int i = x; i < (n); ++i)
+#define fast() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ll fac(int n){
+    ll ans=1;
+    for (int i = 1; i <= n; ++i) {
+        ans*=i;
+    }
+    return ans;
+}
+void solve(){
+int x1,x2,y1,y2;
+cin>>x1>>y1>>x2>>y2;
+cout<<fixed<<setprecision(9)<<sqrt(pow(x2-x1,2)+ pow(y2-y1,2));
+}
+void solve1() {
+    int r1, c1;
+    cin >> r1 >> c1;
+    int arr[r1][c1];
+    f(0, r1)for (int j = 0; j < c1; ++j) {
+            cin >> arr[i][j];
+        }
+    int r2, c2;
+    cin >> r2 >> c2;
+    int arr1[r2][c2];
+    f(0, r2) {
+        for (int j = 0; j < c2; ++j) {
+            cin >> arr1[i][j];
+        }
+    }
+    int arr2[r1][c2];
+    /* for (int i = 0; i < ; ++i) {
+         for (int j = 0; j < ; ++j) {
+             for (int k = 0; k < ; ++k) {
+
+             }
+         }
+     }
+    */
+}
+
+int main() {
+
+    fast()
+    int t = 1;
+    //cin>>t;
+    while (t--) {
+    solve();
+
+    }
+    return 0;
+}
