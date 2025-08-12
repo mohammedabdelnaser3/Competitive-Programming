87d7ed6065d081641153b0e9679313a0fd82f1fc
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:23:02 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/SPOJ/TRYCOMP/50580974_AC_3650ms_292864kB.cpp
@@ -0,0 +1,140 @@
+/*
+*  Author:
+*
+* ╭━╮╭━╮╱╱╭━━━╮╭━━╮╱╭━━━╮╭━━━╮╭╮╱╱╱╭━╮╱╭╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮
+* ┃┃╰╯┃┃╱╱┃╭━╮┃┃╭╮┃╱╰╮╭╮┃┃╭━━╯┃┃╱╱╱┃┃╰╮┃┃┃╭━╮┃┃╭━╮┃┃╭━╮┃┃╭━━╯┃╭━╮┃
+* ┃╭╮╭╮┃╱╱┃┃╱┃┃┃╰╯╰╮╱┃┃┃┃┃╰━━╮┃┃╱╱╱┃╭╮╰╯┃┃┃╱┃┃┃╰━━╮┃╰━━╮┃╰━━╮┃╰━╯┃
+* ┃┃┃┃┃┃╱╱┃╰━╯┃┃╭━╮┃╱┃┃┃┃┃╭━━╯┃┃╱╭╮┃┃╰╮┃┃┃╰━╯┃╰━━╮┃╰━━╮┃┃╭━━╯┃╭╮╭╯
+* ┃┃┃┃┃┃╭╮┃╭━╮┃┃╰━╯┃╭╯╰╯┃┃╰━━╮┃╰━╯┃┃┃╱┃┃┃┃╭━╮┃┃╰━╯┃┃╰━╯┃┃╰━━╮┃┃┃╰╮
+* ╰╯╰╯╰╯╰╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰━━━╯╰╯╱╰━╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰╯╰━╯
+*/
+
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+
+int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dx4[] = {-1, 0, 0, 1};
+int dy4[] = {0, -1, 1, 0};
+#define _ 0
+#define endl '\n'
+#define ll long long
+#define pii pair<int,int>
+#define ull unsigned long long
+#define all(X) (X).begin(),(X).end()
+#define allr(X) (X).rbegin(),(X).rend()
+#define Take(X) for (auto &it:(X)) cin >> it;
+#define Out(X) for (auto &it:(X))cout << it << " ";
+#define TAKE(X, S) for (auto &it:(X)) cin >> it, (S) += it;
+#define Fast_IO ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
+const int mod = 998244353;
+const double pi = acos(-1);
+const ll oo = LLONG_MAX;
+const int OO = 0x7f7f7f7f;
+
+ll Lcm(ll a, ll b) { return a * (b / __gcd(a, b)); }
+
+int mult(int a, int b) { return (1LL * a * b) % mod; }
+
+int mult(int a, int b, int mm) { return (1LL * a * b) % mm; }
+
+int modSum(ll a, ll b) {
+    a = (a % mod + mod) % mod, b = (b % mod + mod) % mod;
+    return (a + b) % mod;
+}
+
+int modSub(ll a, ll b) { return modSum(a, -b); }
+//___________________________________________________________________________
+
+#define int ll
+typedef vector<ll> vi;
+typedef vector<pii > vpi;
+typedef vector<ll> vl;
+#define ld long double
+const int N = 2e5 + 5;
+# define CHILDREN 26
+# define MAX 100
+
+struct Trie{
+    map<char, Trie>children;
+    int isWord = 0;
+    pair<int,string> mx = {0, ""};
+
+    pair<int, string> insert(const string& s, int pos){
+        if(pos != s.size()) {
+            auto p = children[char(s[pos])].insert(s, pos + 1);
+            if(p.first > mx.first)
+                mx = p;
+            else if(p.first == mx.first)
+                mx = (p.second < mx.second ? p : mx);
+            return mx;
+        }
+        else{
+            ++isWord;
+            if (isWord > mx.first)
+                mx = {isWord, s};
+            else if (isWord == mx.first)
+                if(s < mx.second)
+                    mx = {isWord, s};
+            return mx;
+        }
+    }
+
+    pair<int, string> search(const string &s, int pos) {
+        if (pos == s.size())
+            return mx;
+
+        if (children.find(s[pos]) != children.end())
+            return children[s[pos]].search(s, pos + 1);
+
+        return {-1, ""};
+    }
+};
+
+void blow_it() {
+    Trie tr;
+    int n, m;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        tr.insert(s, 0);
+    }
+
+    cin >> m;
+    while (m--){
+        string s;
+        cin >> s;
+        auto ans = tr.search(s, 0);
+        if(ans.first == -1)
+            cout << -1 << '\n';
+        else
+            cout << ans.second << ' ' << ans.first << '\n';
+    }
+}
+
+void preprocess() {
+}
+
+int32_t main() {
+    Fast_IO;
+//    freopen("censor.in", "r", stdin);
+//    freopen("censor.out", "w", stdout);
+//    cout << fixed << sextprecision(9);
+    int t = 1, cases = 1;
+//    cin >> t;
+    preprocess();
+    while (t--) {
+        // cout << "Case #" << cases++ << ": ";
+        blow_it();
+    }
+    return (0 ^ _ ^ 0);
+}
