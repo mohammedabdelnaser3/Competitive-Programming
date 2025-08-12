19bdee67ac6f92e6562dfced3842be088408ecc1
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:21:08 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/AtCoder/abc293_g/59832621_AC_617ms_14616kB.cpp
@@ -0,0 +1,161 @@
+/*
+*  Author:
+*
+* ╭━╮╭━╮╱╱╭━━━╮╭━━╮╱╭━━━╮╭━━━╮╭╮╱╱╱╭━╮╱╭╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮
+* ┃┃╰╯┃┃╱╱┃╭━╮┃┃╭╮┃╱╰╮╭╮┃┃╭━━╯┃┃╱╱╱┃┃╰╮┃┃┃╭━╮┃┃╭━╮┃┃╭━╮┃┃╭━━╯┃╭━╮┃
+* ┃╭╮╭╮┃╱╱┃┃╱┃┃┃╰╯╰╮╱┃┃┃┃┃╰━━╮┃┃╱╱╱┃╭╮╰╯┃┃┃╱┃┃┃╰━━╮┃╰━━╮┃╰━━╮┃╰━╯┃
+* ┃┃┃┃┃┃╱╱┃╰━╯┃┃╭━╮┃╱┃┃┃┃┃╭━━╯┃┃╱╭╮┃┃╰╮┃┃┃╰━╯┃╰━━╮┃╰━━╮┃┃╭━━╯┃╭╮╭╯
+* ┃┃┃┃┃┃╭╮┃╭━╮┃┃╰━╯┃╭╯╰╯┃┃╰━━╮┃╰━╯┃┃┃╱┃┃┃┃╭━╮┃┃╰━╯┃┃╰━╯┃┃╰━━╮┃┃┃╰╮
+* ╰╯╰╯╰╯╰╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰━━━╯╰╯╱╰━╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰╯╰━╯
+*
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+/*
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+*/
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg(Head H, Tail... T) { cerr << ' ' << H; dbg(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dx4[] = {-1, 0, 0, 1};
+int dy4[] = {0, -1, 1, 0};
+#define _ 0
+#define endl '\n'
+#define ll long long
+#define pii pair<int,int>
+#define ull unsigned long long
+#define all(X) (X).begin(),(X).end()
+#define allr(X) (X).rbegin(),(X).rend()
+#define Take(X) for (auto &it:(X)) cin >> it;
+#define Out(X) for (auto &it:(X))cout << it << " ";
+#define TAKE(X, S) for (auto &it:(X)) cin >> it, (S) += it;
+#define Fast_IO ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
+
+const int mod = 1e9 + 7;
+const double pi = acos(-1);
+const ll oo = LLONG_MAX;
+const int OO = 0x7f7f7f7f;
+/*___________________________________________________________________________*/
+#define int long long
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int,int>> vpi;
+#define ld long double
+const int N = 2e5 + 5;
+const int len = (int) sqrt(N + .0) + 1;
+int fact[N];
+
+struct query{
+    int l, r, i;
+
+    bool operator<(query &other){
+        return l / len != other.l / len ?
+         l / len < other.l / len : r < other.r;
+    }
+};
+
+int n, q, k;
+int arr[N], out[N], freq[N];
+query queries[N];
+int curr = 0;
+
+ll comb3(int n) {
+    if (n < 3) return 0;
+    return (ll)n * (n - 1) * (n - 2) / 6;
+}
+
+void add(int i) {
+    int x = arr[i];
+    if (x < 0 || x >= N) return; // Bounds check
+    curr -= comb3(freq[x]);
+    freq[x]++;
+    curr += comb3(freq[x]);
+}
+
+void remove(int i) {
+    int x = arr[i];
+    if (x < 0 || x >= N) return; // Bounds check
+    curr -= comb3(freq[x]);
+    freq[x]--;
+    curr += comb3(freq[x]);
+}
+
+void blow_it()  {
+    cin >> n >> q;
+
+    for (int i = 1; i <= n; ++i)
+        cin >> arr[i];
+    
+    // cin >> q;
+    for (int i = 0; i < q; ++i){
+        cin >> queries[i].l >> queries[i].r;
+        queries[i].l, queries[i].r;
+        queries[i].i = i;
+    }
+    sort(queries, queries + q);
+    int s = 1, e = 0;
+    for (int i = 0; i < q; ++i){
+        int l = queries[i].l, r = queries[i].r, j = queries[i].i;
+        // dbg(l, r);
+        while(s > l)
+            --s, add(s);
+        while(e < r)
+            ++e, add(e);
+
+        while(s < l)
+            remove(s), s++;
+        while(e > r)
+            remove(e), e--;
+
+        out[j] = curr;
+    }
+
+    for (int i = 0; i < q; ++i)
+        cout << out[i] << '\n';
+}
+
+void preprocess() {
+    fact[0] = fact[1] = 1;
+    for (int i = 2; i < N; ++i)
+        fact[i] = i * fact[i - 1];
+    
+}
+
+int32_t main() {
+    Fast_IO;
+    freopen("erorr.txt", "w", stderr);
+   // freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+//    cout << fixed << setprecision(9);
+    int t = 1, cases = 1;
+    // cin >> t;
+    
+    //std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+   preprocess();
+    while (t--) {
+        //cout << "Case #" << cases++ << ':' << endl;
+        blow_it();
+    }
+    //std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    //cerr << "Time : " << (end - start).count() / (1000000.0l) << " ms \n";
+    return (0 ^ _ ^ 0);
+}
