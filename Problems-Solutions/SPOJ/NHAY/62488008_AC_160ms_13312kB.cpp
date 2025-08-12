56d87eb6ea672fa8f29f311fc05ad85623295562
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:22:17 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/SPOJ/NHAY/62488008_AC_160ms_13312kB.cpp
@@ -0,0 +1,175 @@
+/*
+*  Author:
+*
+* ╭━╮╭━╮╱╱╭━━━╮╭━━╮╱╭━━━╮╭━━━╮╭╮╱╱╱╭━╮╱╭╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮╭━━━╮
+* ┃┃╰╯┃┃╱╱┃╭━╮┃┃╭╮┃╱╰╮╭╮┃┃╭━━╯┃┃╱╱╱┃┃╰╮┃┃┃╭━╮┃┃╭━╮┃┃╭━╮┃┃╭━━╯┃╭━╮┃
+* ┃╭╮╭╮┃╱╱┃┃╱┃┃┃╰╯╰╮╱┃┃┃┃┃╰━━╮┃┃╱╱╱┃╭╮╰╯┃┃┃╱┃┃┃╰━━╮┃╰━━╮┃╰━━╮┃╰━╯┃
+* ┃┃┃┃┃┃╱╱┃╰━╯┃┃╭━╮┃╱┃┃┃┃┃╭━━╯┃┃╱╭╮┃┃╰╮┃┃┃╰━╯┃╰━━╮┃╰━━╮┃┃╭━━╯┃╭╮╭╯
+* ┃┃┃┃┃┃╭╮┃╭━╮┃┃╰━╯┃╭╯╰╯┃┃╰━━╮┃╰━╯┃┃┃╱┃┃┃┃╭━╮┃┃╰━╯┃┃╰━╯┃┃╰━━╮┃┃┃╰╮
+* ╰╯╰╯╰╯╰╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰━━━╯╰╯╱╰━╯╰╯╱╰╯╰━━━╯╰━━━╯╰━━━╯╰╯╰━╯
+*
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+/*
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        new_data_set;
+*/
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg(Head H, Tail... T) { cerr << ' ' << H; dbg(T...); }
+#ifndef ONLINE_JUDGE
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dx4[] = {-1, 0, 0, 1};
+int dy4[] = {0, -1, 1, 0};
+#define _ 0
+#define endl '\n'
+#define ll long long
+#define pii pair<int,int>
+#define ull unsigned long long
+#define all(X) (X).begin(),(X).end()
+#define allr(X) (X).rbegin(),(X).rend()
+#define Take(X) for (auto &it:(X)) cin >> it;
+#define Out(X) for (auto &it:(X))cout << it << " ";
+#define TAKE(X, S) for (auto &it:(X)) cin >> it, (S) += it;
+#define Fast_IO ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr)
+
+const double pi = acos(-1);
+const ll oo = LLONG_MAX;
+const int OO = 0x7f7f7f7f;
+/*___________________________________________________________________________*/
+#define int long long
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pair<int,int>> vpi;
+#define ld long double
+
+const int mod = 1e9 + 253;
+const int N = 1e6 + 5, base = 53;
+vector<int> power;
+
+int Power(int a, int b){
+    int res = 1;
+    a = a % mod; 
+    while (b > 0) {
+        if (b % 2 == 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b / 2;
+    }
+
+    return res;
+}
+
+int rng(int l, int r) {
+    static std::mt19937 gen(
+        std::chrono::steady_clock::now().time_since_epoch().count());
+    return std::uniform_int_distribution<long long>(l, r)(gen);
+}
+
+void precomputePowers(int n) {
+    power.resize(n + 1);
+    power[0] = 1;
+    for (int i = 1; i <= n; i++)
+        power[i] = (1LL * power[i - 1] * base) % mod;
+    
+}
+int mode_inv(int b, int m){
+    return Power(b, mod - 2);
+}
+void add_front(int &hash, char c){
+    hash = (hash * base + c) % mod;
+}
+
+void add_back(int &hash, char c, int size){
+    hash += (c * power[size]) % mod;
+    hash %=mod;
+}
+
+void remove_front(int &hash, char c){
+    hash = ((hash - c + mod) % mod) * mode_inv(base, mod) % mod;
+}
+
+void remove_back(int &hash, char c, int size){
+    hash = (hash - (c * power[size] % mod) + mod) % mod;
+}
+
+void blow_it()  {
+    string t, s;
+    int m;
+    while(cin >> m >> t >> s){
+    int n, tHash = 0, sHash = 0;
+        dbg(t, s);
+        n = s.size();
+        // m = t.size();
+        precomputePowers(n);
+        vi ans;
+        for (int i = 0; i < m ; ++i)
+            add_front(tHash, t[i]);
+        dbg(tHash);
+            // tHash = (tHash + Power(t[i] + k, h)) % mod;
+
+        int len = 0, cnt = 0;
+            for (int i = 0; i < n; ++i)
+                if(i < m){
+                    add_front(sHash, s[i]);
+                    len++;
+                }else{
+                    if(sHash == tHash){
+                        cout << i - m << '\n';
+                    }
+                    remove_back(sHash, s[i - m], m - 1);
+                    add_front(sHash, s[i]);
+                }
+            if(sHash == tHash)
+                cout << n - m<< '\n';
+    
+        if(ans.empty())
+             cout << "\n";
+        // cout << cnt << '\n';
+    }
+    for (int i = 0; i < 1e7; ++i){
+        i *= 1;
+    }
+}
+
+void preprocess() {
+
+}
+
+int32_t main() {
+    Fast_IO;
+    freopen("erorr.txt", "w", stderr);
+   // freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+//    cout << fixed << setprecision(9);
+    int t = 1, cases = 1;
+    // cin >> t;
+    
+    //std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+   preprocess();
+    while (t--) {
+        //cout << "Case #" << cases++ << ':' << endl;
+        blow_it();
+    }
+    //std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    //cerr << "Time : " << (end - start).count() / (1000000.0l) << " ms \n";
+    return (0 ^ _ ^ 0);
+}
