199f8e9499de28e753258e792837453d26c900c3
mohammedabdelnaser3 <58954921+mohammedabdelnaser3@users.noreply.github.com>
2025-08-13 00:24:26 +0300

    Add VJudge problem solutions

---
+++ Problems-Solutions/Gym/248968Q/35817350_AC_15ms_8kB.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+#define f(x,n) for(int i = x; i < (n); ++i)
+#define fast() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ll fac(int n){
+    ll ans=1;
+    for (int i = 1; i <= n; ++i) {
+        ans*=i;
+    }
+    return ans;
+}
+void solve(){
+ll a,b;
+cin>>a>>b;
+      cout<<(a*(a-1))/2+(b*(b-1))/2;
+}
+void solve1(){
+    ll n,ans;
+    cin>>n;
+    ll lo=1,hi=1414213562,md;
+    while (lo<=hi){
+        md=lo+(hi-lo)/2;
+        if((md*(md+1))/2<=n){
+            ans=md;
+            lo=md+1;
+        }
+        else
+            hi=md-1;
+    }
+    cout<<ans;
+}
+int main() {
+
+    fast()
+    int t = 1;
+    //cin>>t;
+    while (t--) {
+    solve();
+
+    }
+    return 0;
+}
